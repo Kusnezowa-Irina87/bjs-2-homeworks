3050da43417bcbb65d24b05a55b9a565df44000c
Irina Shtaup <irenmars@mail.ru>
2022-01-12 12:07:38 +0100

    Внесено новое решение

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,8 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  if (arr1.length === arr2.length) {
-    result = arr1.every((element,index) => element === arr2[index]);
-  } else {
-    result = false;
-  }
-
-  return result;
+  return arr1.length === arr2.length && arr1.every((element,index) => element === arr2[index]);;
 }
 
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number*10);
-
-  return resultArr; // array
+  return arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number*10);; // array
 }
